1c4382a674fcef8ac1b250064848a4534ddd7015
Mika Smith <mika.e.smith@gmail.com>
2017-09-12 14:36:19 +1200

    Cleaned up code and added coments

--- JoinedUpWriting.java
+++ JoinedUpWriting.java
@@ -12,7 +12,7 @@ import java.io.*;
  * prefix of the next. 
  * 
  * Returns for a given dictionary, a shortest sequence of 
- * joined up words that link ka beginning word to an end word. 
+ * joined up words that link a beginning word to an end word. 
  */
 
 public class JoinedUpWriting{
@@ -38,7 +38,6 @@ public class JoinedUpWriting{
 	
 	//The common part is at least half as long as one of the two words
 	public static String singlyJoined(String a, String b){
-		//int minCommonLength = Math.min(a.length(), b.length())/2; 
 		StringBuilder result = new StringBuilder(); 
 		StringBuilder chainBuilder = new StringBuilder(); 
 		String chain = findSingleChain(a, b, chainBuilder); 
@@ -55,66 +54,55 @@ public class JoinedUpWriting{
 		}
 	}
 	
-	
+	//This method recursively finds a chain of singly joined strings from 
+	//the starting word to END 
 	public static String findSingleChain(String a, String END, StringBuilder res){
 		String xTarget;
 		String target; 
 		String endTarget;
 		int minSize;
 		
-		//if we have looked through the whole dict, stop. 
-		
-		//if a word whose suffix is a prefix of END, stop. 
-		
-		//for every word in dictionary
-			//find a word X which starts with the same minSize or more letters as A ends with. 
-				//if found, 
-					//append the word to result and increment count. 
-					//use this new word as A and find another word X -call recursively until we find word end. 
-				//if not found, keep looping to the next word
-		
-		//look at last minSize letters or greater of a; 
-		
-		
-		//minsize should be defined in this method! It changes with every word pair!
-		
-		//seeing different lengths of common part - OR should I count down?
-		
-		//two seperate for loops for checking if target equals? 
 		minSize = Math.min(a.length(), END.length())/2; 
-		//to the end of the smaller word
+		//STOPPING CASE - if a word whose suffix is a prefix of END, stop. 
 		for(int i=minSize; i< Math.min(a.length(), END.length()); i++){
-			System.out.println("Min size is " + i);
+		//	System.out.println("Min size is " + i);
 			target = a.substring(a.length()-i, a.length());  //(inclusive, exclusive) suffix
-			System.out.println("Target is " + target);
+		//	System.out.println("Target is " + target);
 			
 			endTarget = (i >= END.length()) ? END : END.substring(0, i); //is this necessary 
 			
 			if(target.equals(endTarget)){//suffix == prefix
-				System.out.println("CHAIN FINISHED \"" + a + "\" matched with \"" + END+"\" with -" + target + "-");
+			//	System.out.println("CHAIN FINISHED \"" + a + "\" matched with \"" + END+"\" with -" + target + "-");
 				singlyCount= singlyCount + 2; //first word is matched to last word
 				return res.toString();  //a = b; stopping case 
 			}
 		}
-		for(String x : dict){
-			//for loop for each length of each word
+		
+		for(String x : dict){ //for every word in dictionary
+			//find a word X which starts with the same minSize or more letters as A ends with. 
+			
 			minSize = Math.min(a.length(), x.length())/2; 
-					
+			//minsize changes with every word pair
 			for(int i=minSize; i < Math.min(a.length(), x.length()); i++){
-				System.out.println("Min size is " + i);
+			//	System.out.println("Min size is " + i);
 				target = a.substring(a.length()-i, a.length());  //(inclusive, exclusive) 
-				System.out.println("Target is " + target);
+			//	System.out.println("Target is " + target);
 				
 				xTarget = (i >= x.length()) ? x : x.substring(0, i);  //is this necessary?
 				
-				if(target.equals(xTarget)){ //then these two words are singly joined 
-					System.out.println("\"" + a + "\" matched with \"" + x + "\" with -" + target + "-");
-					res.append(x +" "); 
+				if(target.equals(xTarget)){ //Found a pair - these two words are singly joined 
+			//		System.out.println("\"" + a + "\" matched with \"" + x + "\" with -" + target + "-");
+					res.append(x +" "); //append the word to the resulting chain
 					singlyCount++; 
 					return findSingleChain(x, END, res);
+					//use this new word as A and find another word X -call recursively until we match END 
 				}
+				//If not found, look at for a larger common part of the same two words
 			}
-		}
+			//if not found, keep looping to the next word
+			
+		}//If we have looked through the whole dictionary, stop and return invalid. 
+		
 		return NOTFOUND; 
 	}
 	
@@ -138,13 +126,16 @@ public class JoinedUpWriting{
 	}
 	
 	public static String findDoubleChain(String a, String END, String intermediate, StringBuilder res){
-		String xTarget;
+		String yPrefix; 
+		String ySuffix;
 		String target; 
 		String endTarget;
+		String prefixTarget; 
+		String suffixTarget; 
 		int minSize; 
 		
+		//STOPPING CASE
 		minSize = (a.length()+END.length())/2; //doubly
-		//minSize = Math.min(a.length(), END.length())/2; - singly
 		for(int i=minSize; i< Math.min(a.length(), END.length()); i++){
 			System.out.println("Min size is " + i);
 			
@@ -159,22 +150,39 @@ public class JoinedUpWriting{
 			}
 		}
 			
-		for(String x : dict){
-			minSize = (a.length()+x.length())/2; //doubly
-		//	minSize = Math.min(a.length(), x.length())/2; singly 
-			for(int i=minSize; i< Math.min(a.length(), x.length()); i++){
-				System.out.println("Min size is " + i);
+		for(String x : dict){ //look for word to pair - A Y X
+			minSize = (a.length()+x.length())/2; //doubly 
+			//for every word in the dictionary, pair it with A and find 
+			//another word Y which is at least minSize long and has its prefix
+			//matching the suffix of A, and its suffix matching the prefix of X
+			System.out.println("MinSize is " + minSize);
+			//need to look at varying lengths
+			
+			//these are WRONG ! 5 in total not 5 per pre/suffix
+			prefixTarget = a.substring(a.length()-minSize, a.length()); //In total!! thi
+			suffixTarget = x.substring(0, minSize); 
+			//minsize is too large? 
+			
+			//need another for loop changing minSize, need to keep track 
+			//that if suffix is 1, prefix is 4 
+			
+			for(String y : dict){ //Look for intermediate words 
 				
-				target = a.substring(a.length()-i, a.length());  //(inclusive, exclusive) 
-				System.out.println("Target is " + target);
-				
-				xTarget = (i >= x.length()) ? x : x.substring(0, i); 
-				
-				if(target.equals(xTarget)){ //then these two words are singly joined 
-					System.out.println("\"" + a + "\" matched with \"" + x + "\" with -" + target + "-");
-					res.append(x +" "); 
-					doublyCount++; 
-					return findDoubleChain(x, END, intermediate, res);
+				if(y.length()>=minSize){
+					
+					//if yprefix matches prefixTarget
+						//check if suffix matches suffixTarget
+					System.out.println(y + " is larger than " + minSize);
+					yPrefix = y.substring(0, minSize); 
+					ySuffix = y.substring((y.length()-minSize), y.length()); 
+					
+					if((yPrefix == prefixTarget) && (ySuffix == suffixTarget)){
+						System.out.println("MATCH"); //the word is a match 
+						res.append(y +" " + x + " "); 
+						doublyCount++; 
+						return findDoubleChain(x, END, y , res);//y or x becomes a???
+							
+					}
 				}
 			}
 		}
